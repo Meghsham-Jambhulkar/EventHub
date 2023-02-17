1051f41f469b5d8ef1cae7369b47b97d8478baa8
Meghsham Jambhulkar <106965064+Meghsham-Jambhulkar@users.noreply.github.com>
2023-02-17 11:01:21 +0530

    Delete src directory

--- src/app/basic.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BasicDirective } from './basic.directive';
-
-describe('BasicDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BasicDirective();
-    expect(directive).toBeTruthy();
-  });
-});
